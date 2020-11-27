4152b783eb38b5bd5b3246f23eb5f53ed7666652
Bartek Karelus <bartek.karelus@gmail.com>
2020-11-27 22:03:15 +0100

    add start function, letters from alphabet

--- App/Game.js
+++ App/Game.js
@@ -1,8 +1,25 @@
 class Game {
+    constructor({ outputWrapper, wordWrapper, categoryWrapper, letterWrapper }) {
+        this.outputWrapper = outputWrapper,
+            this.wordWrapper = wordWrapper,
+            this.categoryWrapper = categoryWrapper,
+            this.letterWrapper = letterWrapper
+    }
 
+    clickButton = (letter) => {
+        console.log(letter)
+    }
 
     start() {
-        console.log("hejka")
+        let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        alphabet.forEach(element => {
+            const button = document.createElement("button")
+            button.textContent = element
+            button.addEventListener("click", () => this.clickButton(element))
+            this.letterWrapper.appendChild(button)
+        })
+
+
     }
 }
 
